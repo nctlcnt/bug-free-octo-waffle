e7da4ac4a96a8252ff1ab2b9733969848a3d77fe
Cecilia Che <moonsue569@gmail.com>
2023-03-01 19:55:27 +0800

    clean up code and add eslint

--- lib/untils.ts
+++ lib/untils.ts
@@ -1,8 +1,6 @@
 export function getMyPosition(mapping, heights, myIndex, colNum) {
   let myPosition = 0
   for (let i = myIndex - colNum; i >= 0; i = i - colNum) {
-    // if (!heights[mapping[i]]) return false
-    // console.log(heights[mapping[i]])
     myPosition += heights[mapping[i]]
     if (myIndex > colNum - 1) {
       myPosition += 10
